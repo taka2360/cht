9f33dc2cf01d983930e67bdbc17376f12579a6cd
taka2360 <132958770+taka2360@users.noreply.github.com>
2023-10-08 10:29:01 +0900

    a

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
+import pickle
+
 
 from gensim.models.word2vec import Word2Vec
 import gensim
 
-model = gensim.models.KeyedVectors.load_word2vec_format('cc.ja.300.vec.gz', binary=False)
-print(model)
+from janome.tokenizer import Tokenizer
+
+t = Tokenizer()
+malist = t.tokenize("私は田舎で育ちました。")
+with open('gensim-kvecs.cc.ja.300.vec.pkl', mode='rb') as fp:
+    model = pickle.load(fp)
+
+print(model.most_similar( [ model["猫"] ], [], 10)[0][0])
+
